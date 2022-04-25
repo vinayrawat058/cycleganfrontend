cf5e9f462c5123d95004806556dd00354b31da3c
Nikhil Shinde <nikhilshinde662@gmail.com>
2022-04-25 12:30:33 +0530

    Added Stylings

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className='container'>
       <DragandDrop/>
     </div>
-    <Fotter/>
+    {/* <Fotter/> */}
     </>
   );
 }
--- src/components/loader/loader.js
+++ src/components/loader/loader.js
@@ -3,6 +3,6 @@ import "./loader.css";
 export default function loader(props){
     return <div>
         <div class="lds-hourglass"></div>
-        <div>Loading...</div>
+        <div>Processing...</div>
     </div>
 }
